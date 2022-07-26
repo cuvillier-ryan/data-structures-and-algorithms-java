dcfd2f251be977c21c7adc155793b724c01cf1cd
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2022-07-25 20:45:43 -0500

    AdjacencyMatrix

--- src/dataStructruresANDAlgorithms/Main.java
+++ src/dataStructruresANDAlgorithms/Main.java
@@ -1,5 +1,6 @@
 package dataStructruresANDAlgorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -34,8 +35,25 @@ public class Main {
 		ap.missingNumber(newArray);
 		SinglyLinkedList sLL = new SinglyLinkedList();
 		sLL.createSinglyLinkedList(4);
-		System.out.println(sLL.head.value
-				);
+		System.out.println(sLL.head.value);
+		
+		// Adjacency Matrix
+		ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+		nodeList.add(new GraphNode("A", 0));
+		nodeList.add(new GraphNode("B", 1));
+		nodeList.add(new GraphNode("C", 2));
+		nodeList.add(new GraphNode("D", 3));
+		nodeList.add(new GraphNode("E", 4));
+		
+		Graph g = new Graph(nodeList);
+		g.addUndirectedEdge(0, 1);
+		g.addUndirectedEdge(0, 2);
+		g.addUndirectedEdge(0, 3);
+		g.addUndirectedEdge(1, 4);
+		g.addUndirectedEdge(2, 3);
+		g.addUndirectedEdge(3, 4);
+		System.out.println(g.toString());
+		
 		
 	}	
 
