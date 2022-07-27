a1429027a5bae1832fa660477b2d648600a30e6a
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2022-07-27 12:16:50 -0500

    GraphNodeList push

---
+++ src/dataStructruresANDAlgorithms/GraphNodeList.java
@@ -0,0 +1,15 @@
+package dataStructruresANDAlgorithms;
+
+import java.util.ArrayList;
+
+public class GraphNodeList {
+	public String name;
+	public int index;
+	
+	public ArrayList<GraphNodeList> neighbors = new ArrayList<GraphNodeList>();
+	
+	public GraphNodeList(String name, int index) {
+		this.name = name;
+		this.index = index;
+	}
+}
